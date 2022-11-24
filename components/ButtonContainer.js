36db93a9f9fd86b1d0f9874d3c281173720eec91
Djuppi <askeda@gmail.com>
2022-11-24 13:43:50 +0100

    Refactor

--- components/ButtonContainer.js
+++ components/ButtonContainer.js
@@ -3,7 +3,6 @@ import styles from '../styles/ButtonContainer.module.css';
 export const ButtonContainer = ({children}) => {
 
     return (
-
         <section className={styles.buttonContainer}>
             {children}
         </section>
